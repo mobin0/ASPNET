55ae53c5e5c226aae259bcda97f0c8aa45f5e3bd
Mobin Skaria <mobin0@berkeley.edu>
2022-07-20 19:18:40 -0700

    MVP Commit

---
+++ Testing/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System;
+namespace Testing.Models
+
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+    }
+}
+
